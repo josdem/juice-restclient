814be78606a8b1aa2a91da2b207bf72a70ef4f37
josdem <joseluis.delacruz@gmail.com>
2024-02-13 12:58:48 -0500

    Adding category model

---
+++ src/main/java/com/josdem/fruitypedia/restclient/model/Category.java
@@ -0,0 +1,13 @@
+package com.josdem.fruitypedia.restclient.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    private Integer id;
+    private String name;
+}
